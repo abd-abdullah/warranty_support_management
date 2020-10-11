1aadd99c4dd5c63e727d0e15528766528412806d
Md. Abdullah <abdullah@systechdigital.com>
2020-10-11 23:15:33 +0600

    fix app name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/logo.jpg')}}">
   <link rel="icon" type="image/png" href="{{asset('img/logo.jpg')}}">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>Laravel Sanctum</title>
+  <title>{{env('APP_NAME')}}</title>
   <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
