907400f4ebe5d410deb744be60d1d0aab4dcd7ce
Md. Abdullah <abdullah@systechdigital.com>
2020-11-05 23:16:29 +0600

    test chunk

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,11 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.webpackConfig({
-    output:{
-        chunkFilename:'js/vuejs_code_split/[name].js',
-    }
-});
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
