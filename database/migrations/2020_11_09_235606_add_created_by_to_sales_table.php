ee7a014e1c482056bc245f5cdead3c635f0683ee
Md. Abdullah <abdullah@systechdigital.com>
2020-11-10 00:12:12 +0600

    added sales created by

---
+++ database/migrations/2020_11_09_235606_add_created_by_to_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCreatedByToSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by')->default(1)->after('next_service_date');
+            $table->unsignedBigInteger('updated_by')->nullable()->after('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->dropForeign(['created_by']);
+            $table->dropColumn('created_by');
+            $table->dropForeign(['updated_by']);
+            $table->dropColumn('updated_by');
+        });
+    }
+}
