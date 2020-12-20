d0b5079ed74a533904e57dac0f7cdb3b4e752f2b
Md. Abdullah <abdullah@systechdigital.com>
2020-12-20 11:55:36 +0600

    commented migrate features

--- routes/web.php
+++ routes/web.php
@@ -13,35 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/migrate_data', function(){
-    $list = [];
-    $sales = \App\Models\Sale::with('customer.user')->get();
-    foreach($sales as $sale){
-        if($sale->customer != NULL){
-            $existing = \App\Models\Sale::where('customerId', $sale->customer->customerId)->first();
-            $data = [
-                'customer_type_id' => $sale->customer->customer_type_id,
-                'customerId' => ($existing == NULL)?$sale->customer->customerId:($sale->customer->customerId.'(update)'.$sale->id),
-                'name' => $sale->customer->user->name,
-                'email' => $sale->customer->user->email,
-                'phone' => $sale->customer->user->phone,
-                'other_contact_numbers' => $sale->customer->user->other_contact_numbers,
-                'country_id' => $sale->customer->user->country_id,
-                'division_id' => $sale->customer->user->division_id,
-                'district_id' => $sale->customer->user->district_id,
-                'upazila_id' => $sale->customer->user->upazila_id,
-                'zone_id' => $sale->customer->user->zone_id,
-                'address' => $sale->customer->user->address,
-            ];
-            $sale->update($data);
-        }
-        else{
-            $sale->forcedelete();
-        }
+// Route::get('/migrate_data', function(){
+//     $list = [];
+//     $sales = \App\Models\Sale::with('customer.user')->get();
+//     foreach($sales as $sale){
+//         if($sale->customer != NULL){
+//             $existing = \App\Models\Sale::where('customerId', $sale->customer->customerId)->first();
+//             $data = [
+//                 'customer_type_id' => $sale->customer->customer_type_id,
+//                 'customerId' => ($existing == NULL)?$sale->customer->customerId:($sale->customer->customerId.'(update)'.$sale->id),
+//                 'name' => $sale->customer->user->name,
+//                 'email' => $sale->customer->user->email,
+//                 'phone' => $sale->customer->user->phone,
+//                 'other_contact_numbers' => $sale->customer->user->other_contact_numbers,
+//                 'country_id' => $sale->customer->user->country_id,
+//                 'division_id' => $sale->customer->user->division_id,
+//                 'district_id' => $sale->customer->user->district_id,
+//                 'upazila_id' => $sale->customer->user->upazila_id,
+//                 'zone_id' => $sale->customer->user->zone_id,
+//                 'address' => $sale->customer->user->address,
+//             ];
+//             $sale->update($data);
+//         }
+//         else{
+//             $sale->forcedelete();
+//         }
 
-    }
-    print_r($list);
-});
+//     }
+//     print_r($list);
+// });
 
 Route::get('{path}','HomeController@allRequest')->where('path', '(.*)');
 Auth::routes();
